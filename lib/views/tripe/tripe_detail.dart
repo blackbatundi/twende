821342fd11c5c88769e107513de7449584df239c
Benjamin batundi <benbatundi7@gmail.com>
2025-02-17 07:19:22 +0100

    refactoring the UI

--- lib/views/tripe/tripe_detail.dart
+++ lib/views/tripe/tripe_detail.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:twende/services/app_setting.dart';
 import 'package:twende/services/extentions.dart';
 import 'package:twende/services/style.dart';
 

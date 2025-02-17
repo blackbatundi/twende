bc4b164d96a37c33ad841c97b074c6e4bc1e0653
Benjamin batundi <benbatundi7@gmail.com>
2025-02-17 07:13:10 +0100

    starting with twende apk refactoring the UI

--- lib/views/tripe/my_ride.dart
+++ lib/views/tripe/my_ride.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:twende/services/extentions.dart';
+import 'package:twende/services/style.dart';
 import 'package:twende/views/tripe/tripe_detail.dart';
 
 class MyRide extends StatefulWidget {
@@ -25,20 +27,17 @@ class _MyRideState extends State<MyRide> {
                 color: Theme.of(context).primaryColor,
               ),
             ),
-            const SizedBox(height: 16),
+          AppStyle.SPACING_LG.heightBox,
             Expanded(
               child: SingleChildScrollView(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      ...List.generate(
-                        15,
-                        (index) => tripe(),
-                      )
-                    ],
-                  ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    ...List.generate(
+                      15,
+                      (index) => tripe(),
+                    )
+                  ],
                 ),
               ),
             ),
